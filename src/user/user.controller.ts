ee85d956bcf2f469479006fb1365b378f36b8c2b
Edilson Nantes Junior <nantes.junior@gmail.com>
2024-11-26 00:30:06 -0300

    feat: Criação UserEntity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -9,12 +9,12 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get()
-    async getAllUsers(): Promise<User[]> {
+    async getAllUsers(): Promise<UserEntity[]> {
         return this.userService.getAllUsers();
     }
 
     @Post()
-    async createUser(@Body() createUser:CreateUserDTO): Promise<User> {
+    async createUser(@Body() createUser:CreateUserDTO): Promise<UserEntity> {
         return this.userService.createUser(createUser);
     }
 }
